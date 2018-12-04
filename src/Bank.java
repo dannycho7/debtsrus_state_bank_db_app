85891f8c4fc268fcac1f7077bbf5900fb114a4d3
dannycho7 <dannycho7@gmail.com>
2018-12-03 16:03:28 -0800

    Commit transaction at the end

--- src/Bank.java
+++ src/Bank.java
@@ -8,16 +8,19 @@ public class Bank {
 		Connection conn = null;
         try {
         	conn = JDBCConnectionManager.getConnection();
-			Customer.create(conn, "123456310", "Danny Cho", "6681 Berkshire Terrace", "1010", false);
-			int account_id = SavingsAccount.create(conn, 500, "Bank of America", "123456310", false);
-        	PocketAccount.create(conn, 500, "Bank of America", "123456310", account_id, false);
-			conn.commit();
+			Customer.create(conn, "123456353", "Danny Cho", "6681 Berkshire Terrace", "1510", false);
+			int account_id = SavingsAccount.create(conn, 500, "Bank of America", "123456353", false);
+        	PocketAccount.create(conn, 500, "Bank of America", "123456353", account_id, false);
+        	conn.commit();
 		} catch (SQLException se) {
 			// Handle errors for JDBC
 			if (conn != null)
 				JDBCConnectionManager.rollbackConn(conn);
 			se.printStackTrace();
-      	} finally {
+        } catch (Exception ex) {
+			JDBCConnectionManager.rollbackConn(conn);
+			ex.printStackTrace();
+		} finally {
 			JDBCConnectionManager.closeConnection(
 				conn,
 				null // statement
