d55ac70474be151b3e22b875d89aff8fe4655a1b
dannycho7 <dannycho7@gmail.com>
2018-12-04 10:17:28 -0800

    Collect impl

--- src/models/transaction/TransactionFactory.java
+++ src/models/transaction/TransactionFactory.java
@@ -205,4 +205,59 @@ public class TransactionFactory {
 
         return t_id;
     }
+
+    public static int createCollect(
+            Connection conn,
+            int amount,
+            String initiator, // customer tax_id
+            int transactor, // pocket account_id
+            int operand, // account_id
+            boolean should_commit
+    ) throws SQLException, IllegalArgumentException {
+        PocketAccount transactor_account = PocketAccount.findOpen(conn, transactor);
+        if (!transactor_account.hasOwner(conn, initiator)) {
+            String err_msg = String.format("The initiator %s does not own the transactor account %d"
+                    , initiator
+                    , transactor
+            );
+            throw new IllegalArgumentException(err_msg);
+        }
+        if (transactor_account.getLinkedAccountId() != operand) {
+            String err_msg = String.format("The account %d is not linked to the pocket account %d"
+                    , operand
+                    , transactor
+            );
+            throw new IllegalArgumentException(err_msg);
+        }
+        CheckSavingsAccountBase linked_account = CheckSavingsAccountBase.findOpen(conn, operand);
+        int fee = (int) (0.03 * amount);
+        fee += transactor_account.hasTransactionThisMonth(conn) ? 0 : 500;
+        String timestamp = BankUtil.getSQLTimeStamp();
+        transactor_account.updateBalance(
+                conn,
+                transactor_account.getBalance() - amount - fee,
+                false // should_commit
+        );
+        linked_account.updateBalance(
+                conn,
+                linked_account.getBalance() + amount,
+                false // should_commit
+        );
+
+        int t_id = BinaryTransaction.create(
+                conn,
+                amount,
+                timestamp,
+                fee,
+                initiator,
+                transactor,
+                BinaryTransaction.BinaryTransactionType.COLLECT,
+                operand,
+                false // should_commit
+        );
+        if (should_commit)
+            conn.commit();
+
+        return t_id;
+    }
 }
