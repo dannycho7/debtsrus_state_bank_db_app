948df9321c69f959b8dc4fc4bc700d1feb506f80
dannycho7 <dannycho7@gmail.com>
2018-12-05 11:28:47 -0800

    ATMAppInterface Deposit

--- src/gui/ATMAppInterface.java
+++ src/gui/ATMAppInterface.java
@@ -1,5 +1,7 @@
 package gui;
 
+import gui.atm_panels.*;
+
 import bank_util.*;
 import models.*;
 import models.account.*;
@@ -11,10 +13,22 @@ import java.sql.*;
 
 public class ATMAppInterface extends JPanel{
     private Connection conn;
+    private final String logged_in_customer_id = "123456789"; // TODO: Remove later for authentication
 
     public ATMAppInterface(Connection conn) {
         super();
         this.conn = conn;
         this.add(new JLabel("ATM App Interface"));
+        this.add(getDepositButton());
+    }
+
+    public JButton getDepositButton() {
+        JButton deposit_btn = new JButton("Deposit");
+        deposit_btn.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                DepositPanel.openDepositDialog(conn, logged_in_customer_id);
+            }
+        });
+        return deposit_btn;
     }
 }
